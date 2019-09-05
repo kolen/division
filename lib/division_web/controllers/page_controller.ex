1f4acb8cd379adca804fcf98bb29de4e3f2d63d2
Andrey Andriychuk <yaidzo@yandex.ru>
2019-09-05 12:30:37 +0300

    Correctly handle

--- lib/division_web/controllers/page_controller.ex
+++ lib/division_web/controllers/page_controller.ex
@@ -3,9 +3,15 @@ defmodule DivisionWeb.PageController do
   alias Division.Chats
   alias Division.Accounts
 
+  plug :get_user_from_session
+
   def index(conn, _params) do
     chats = Chats.list_chats()
     users = Accounts.list_users()
     render(conn, "index.html", chats: chats, users: users)
+  end
+
+  defp get_user_from_session(conn, _) do
+    DivisionWeb.Plugs.Auth.get_user_from_session(conn, {})
   end
 end
--- lib/division_web/plugs/auth.ex
+++ lib/division_web/plugs/auth.ex
@@ -8,16 +8,24 @@ defmodule DivisionWeb.Plugs.Auth do
 
   def call(conn, _opts) do
     user_id = Plug.Conn.get_session(conn, :current_user_id)
+    conn = get_user_from_session(conn, {})
 
-    if user_id do
-      current_user = Accounts.get_user!(user_id)
-
-      conn
-      |> assign(:current_user, current_user)
-    else
+    unless conn.assigns do
       conn
       |> redirect(to: "/login")
       |> halt()
+    else
+      conn
+    end
+  end
+
+  def get_user_from_session(conn, _) do
+   user_id = Plug.Conn.get_session(conn, :current_user_id)
+    if user_id do
+      current_user = Accounts.get_user!(user_id)
+      conn |> assign(:current_user, current_user)
+    else
+      conn
     end
   end
 end
