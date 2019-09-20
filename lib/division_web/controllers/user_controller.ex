ef0981a1b33e6bb666887e9d44ec565a7f7a5736
Andrey Andriychuk <yaidzo@yandex.ru>
2019-09-20 23:20:51 +0300

    Update avatar after create the user

--- lib/division_web/controllers/user_controller.ex
+++ lib/division_web/controllers/user_controller.ex
@@ -17,6 +17,7 @@ defmodule DivisionWeb.UserController do
   def create(conn, %{"user" => user_params}) do
     case Accounts.create_user(user_params) do
       {:ok, user} ->
+        Accounts.update_user(user, %{"avatar" => user_params["avatar"]})
         conn
         |> put_session(:current_user_id, user.id)
         |> put_flash(:info, "Registered successfully.")
